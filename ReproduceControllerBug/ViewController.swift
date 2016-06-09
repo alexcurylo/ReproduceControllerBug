52d1a83d95778fcae9f546eacd8302dfeb535e94
simon.bcr@gmail.com <simon.bcr@gmail.com>
2016-06-09 18:50:44 +0700

    Initial Commit

---
+++ ReproduceControllerBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReproduceControllerBug
+//
+//  Created by Simon on 6/9/16.
+//  Copyright © 2016 Simon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
