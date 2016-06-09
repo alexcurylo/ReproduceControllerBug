1153607f9bfe40ed4d37241aad350f1726b28956
Alex Curylo <alex@alexcurylo.com>
2016-06-09 13:11:39 -0700

    Show 10 red cells
    Just for some visible verification of correct operation.

--- ReproduceControllerBug/SwiftCollectionViewController.swift
+++ ReproduceControllerBug/SwiftCollectionViewController.swift
@@ -52,20 +52,21 @@ class SwiftCollectionViewController: UICollectionViewController {
 
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 10
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath)
     
         // Configure the cell
-    
+        cell.backgroundColor = UIColor.redColor()
+        
         return cell
     }
 
